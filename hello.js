733f15583edcf18a73b59ea09f42607b33c4d527
ramin sharafkandi <rsharafkandi@gmail.com>
2021-05-02 13:00:24 +0430

    first commit

---
+++ hello.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer1 = document.querySelector('#a_typical_container');
+ReactDOM.render(e(LikeButton), domContainer1);
+
